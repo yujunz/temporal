d9322b5e55505c9520342c5458828e0c7bcdc5cb
wenquan xing <wenquan.xing@gmail.com>
2023-05-11 13:49:16 -0700

    TEST ONLY -- Enable replication stream -- TEST ONLY

--- service/history/configs/config.go
+++ service/history/configs/config.go
@@ -324,7 +324,7 @@ func NewConfig(
 	cfg := &Config{
 		NumberOfShards: numberOfShards,
 
-		EnableReplicationStream: dc.GetBoolProperty(dynamicconfig.EnableReplicationStream, false),
+		EnableReplicationStream: dc.GetBoolProperty(dynamicconfig.EnableReplicationStream, true),
 
 		RPS:                                   dc.GetIntProperty(dynamicconfig.HistoryRPS, 3000),
 		MaxIDLengthLimit:                      dc.GetIntProperty(dynamicconfig.MaxIDLengthLimit, 1000),
